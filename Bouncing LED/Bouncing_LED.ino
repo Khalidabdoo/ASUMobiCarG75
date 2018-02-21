ecaa53eadc2f59b353b10556a2bc32f89515bce9
Khalidabdoo <36649694+Khalidabdoo@users.noreply.github.com>
2018-02-21 17:45:57 +0200

    Add files via upload

---
+++ Bouncing LED/Bouncing_LED.ino
@@ -0,0 +1,27 @@
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 4; i < 8; i++) {
+    digitalWrite(i, 1);
+    delay(1000);
+    digitalWrite(i, 0);
+
+  }
+  for (int j = 6; j > 4; j--) {
+    digitalWrite(j, 1);
+    delay(1000);
+    digitalWrite(j, 0);
+
+
+  }
+}
+
